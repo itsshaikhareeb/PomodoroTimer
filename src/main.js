a0b7c9b5bbfd059cbc1137db64edd043c68e44a9
Shaikh Areeb <shaikhareeb321@gmail.com>
2026-05-22 20:15:25 +0530

    add sessionEnding functionality

--- src/main.js
+++ src/main.js
@@ -14,10 +14,13 @@ document.querySelector('#app').innerHTML = `
   <button id="long-break-btn">Long Break</button>
 
 </div>
-
+<h2 id="mode">Focus Time</h2>
     <div class="timer" >
       25:00
     </div>
+    <p id="session-count">
+      Completed Sessions: 0
+    </p>
 
     <div class="buttons">
       <button class="reset-btn btn" > Reset</button>
@@ -36,18 +39,19 @@ const pauseBtn = document.querySelector(".pause-btn");
 const resetBtn = document.querySelector(".reset-btn");
 
 
-let timeLeft = 6;
+let timeLeft = 0;
 let isRunning = false;
 let currentMode = "focus";
+let completedPomodoros = 0;
 
 
-function updateTimer(){ 
-  const minutes = Math.floor(timeLeft/60);
+function updateTimer() {
+  const minutes = Math.floor(timeLeft / 60);
 
   const seconds = String(timeLeft % 60).padStart(2, "0");
 
   timerEle.innerText = `${minutes}:${seconds}`
-} 
+}
 updateTimer()
 
 let intervalId;
@@ -56,39 +60,52 @@ startBtn.addEventListener("click", () => {
   console.log("Started");
   if (isRunning) return;
   isRunning = true;
-  
+
   intervalId = setInterval(() => {
-    if(timeLeft > 0){
+    if (timeLeft > 0) {
 
-    timeLeft--;
-    updateTimer();
-  }
+      timeLeft--;
+      updateTimer();
+    }
 
-  else{
-    isRunning =false;
-    clearInterval(intervalId) 
-  }
-}, 1000);
+    else {
+
+      clearInterval(intervalId);
+
+      isRunning = false;
+
+      handleSessionEnd();
+
+    }
+  }, 1000);
 
 });
- 
-pauseBtn.addEventListener("click",()=>{
+
+pauseBtn.addEventListener("click", () => {
   console.log("Paused");
   clearInterval(intervalId);
-  isRunning =false;
+  isRunning = false;
 })
 
-resetBtn.addEventListener("click",()=>{
-  clearInterval(intervalId)
-  
-  timeLeft = 10;
-  updateTimer()
-  isRunning= false
-})
+const sessionCountEl = document.querySelector("#session-count");
+resetBtn.addEventListener("click", () => {
+
+  clearInterval(intervalId);
+
+  isRunning = false;
+
+  completedPomodoros = 0;
+
+  sessionCountEl.innerText =
+    "Completed Sessions: 0";
+
+  switchMode("focus");
+
+});
 
 
 
-const modeEl = document.querySelector(".modes");
+const modeEl = document.querySelector("#mode");
 
 const focusBtn = document.querySelector("#focus-btn");
 
@@ -96,38 +113,71 @@ const shortBreakBtn = document.querySelector("#short-break-btn");
 
 const longBreakBtn = document.querySelector("#long-break-btn");
 
-function switchMode(mode){
-  clearTimeout(intervalId);
+
+function switchMode(mode) {
+  clearInterval(intervalId);
   isRunning = false;
 
   currentMode = mode;
 
-  if(mode === "focus"){
-    timeLeft = 50
+  if (mode === "focus") {
+    timeLeft = 7
     modeEl.innerText = "Focus Time";
-  } 
+  }
 
-  else if(mode === "ShortBreak"){
-    timeLeft = 20;
+  else if (mode === "shortBreak") {
+    timeLeft = 3;
     modeEl.innerText = "Short Break";
   }
 
-  else if(mode ==="longBreak"){
-    timeLeft = 30;
+  else if (mode === "longBreak") {
+    timeLeft = 5;
     modeEl.innerText = "Long Break";
   }
   updateTimer();
 }
 
-focusBtn.addEventListener("click",()=>{
+function handleSessionEnd() {
+
+  if (currentMode === "focus") {
+
+    completedPomodoros++;
+
+    sessionCountEl.innerText =
+      `Completed Sessions: ${completedPomodoros}`;
+
+    if (completedPomodoros % 4 === 0) { //after every 4 focus sessions..
+
+      switchMode("longBreak");
+
+    } else {
+
+      switchMode("shortBreak");
+
+    }
+
+  }
+
+  else {
+
+    switchMode("focus");
+
+  }
+
+}
+
+
+
+
+focusBtn.addEventListener("click", () => {
   switchMode("focus");
 })
-shortBreakBtn.addEventListener("click",()=>{
-  switchMode("ShortBreak");
+shortBreakBtn.addEventListener("click", () => {
+  switchMode("shortBreak");
 })
-longBreakBtn.addEventListener("click",()=>{
+longBreakBtn.addEventListener("click", () => {
   switchMode("longBreak");
 })
- 
- 
- 
+
+
+
